289a16313c41937eae0995090ac0c0f4756c6500
Antonio Ruano <blinx2500@gmail.com>
2021-10-06 21:11:08 +0200

    Estructuración de menú de ajustes y acceso

--- QuizGame/app/src/main/java/com/mrwojack/quizgame/MainActivity.java
+++ QuizGame/app/src/main/java/com/mrwojack/quizgame/MainActivity.java
@@ -2,13 +2,32 @@ package com.mrwojack.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Eventos de ciclo de la aplicación //
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Eventos de cambio de actividad //
+    /**
+     *
+     * @param view
+     */
+    public void openSettingsMenu(View view) {
+        // Creación de objeto intent
+        Intent int_settingsM = new Intent(this, SettingsActivity.class);
+
+        startActivity(int_settingsM);
     }
 }
