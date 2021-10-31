491239aa0848007e1a4b3bfd69fc1079c13aa127
Nachete07 <ignaciocallesicq@gmail.com>
2021-10-31 13:30:09 +0100

    Base de datos chidosa

--- QuizGame/app/src/main/java/com/mrwojack/gameopardy/DbHelper.java
+++ QuizGame/app/src/main/java/com/mrwojack/gameopardy/DbHelper.java
@@ -1,9 +1,12 @@
 package com.mrwojack.gameopardy;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.mrwojack.gameopardy.QuizContract.*;
+import com.mrwojack.gameopardy.fragments.questions.NormalQuestion;
+
 import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
@@ -14,7 +17,7 @@ public class DbHelper extends SQLiteOpenHelper {
 
 
 
-    public DbHelper(@Nullable Context context, ) {
+    public DbHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -23,14 +26,14 @@ public class DbHelper extends SQLiteOpenHelper {
         this.db = db;
 
         final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
-              QuizContract.QuestionsTable.TABLE_NAME + "(" +
+              QuizContract.QuestionsTable.TABLE_NAME + " (" +
                 QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 QuestionsTable.COLUMN_QUESTION + " TEXT, " +
                 QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
                 QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
                 QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
                 QuestionsTable.COLUMN_OPTION4 + " TEXT, " +
-                QuestionsTable.COLUMN_ANSWER + " INTEGER, " + ")";
+                QuestionsTable.COLUMN_ANSWER + " INTEGER " + ")";
 
 
         db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
@@ -38,13 +41,28 @@ public class DbHelper extends SQLiteOpenHelper {
     }
 
    private void fillQuestionsTable(){
-       Question
-   }
+
+
+        NormalQuestion q1 = new NormalQuestion("¿?", "A", "B", "C", "D", 1);
+        addQuestion(q1);
+
+    }
+
+    private void addQuestion(NormalQuestion normalquestion){
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionsTable.COLUMN_QUESTION, normalquestion.getQuestion());
+        cv.put(QuestionsTable.COLUMN_OPTION1, normalquestion.getOption1());
+        cv.put(QuestionsTable.COLUMN_OPTION2, normalquestion.getOption2());
+        cv.put(QuestionsTable.COLUMN_OPTION3, normalquestion.getOption3());
+        cv.put(QuestionsTable.COLUMN_OPTION4, normalquestion.getOption4());
+        cv.put(QuestionsTable.COLUMN_ANSWER, normalquestion.getAnswer());
+        db.insert(QuestionsTable.TABLE_NAME, null, cv);
+    }
 
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-db.execSQL("DROP TABLE IF EXISTS" + QuestionsTable.TABLE_NAME);
-onCreate(db);
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionsTable.TABLE_NAME);
+        onCreate(db);
     }
 }
--- QuizGame/app/src/main/java/com/mrwojack/gameopardy/QuizContract.java
+++ QuizGame/app/src/main/java/com/mrwojack/gameopardy/QuizContract.java
@@ -4,15 +4,17 @@ import android.provider.BaseColumns;
 
 
 public final class QuizContract{
-    public class QuestionsTable implements BaseColumns {
 
+    private QuizContract(){}
+
+    public class QuestionsTable implements BaseColumns {
         public static final String TABLE_NAME = "quiz_normal_questions";
-        public static final String COLUMN_QUESTION = "questions";
+        public static final String COLUMN_QUESTION = "question";
         public static final String COLUMN_OPTION1 = "option1";
         public static final String COLUMN_OPTION2 = "option2";
         public static final String COLUMN_OPTION3 = "option3";
         public static final String COLUMN_OPTION4 = "option4";
-        public static final String COLUMN_ANSWER = "answer_nr";
+        public static final String COLUMN_ANSWER = "answer";
 
     }
 }
