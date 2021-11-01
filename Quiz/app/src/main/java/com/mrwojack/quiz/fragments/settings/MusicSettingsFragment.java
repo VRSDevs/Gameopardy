9c41011f9eba12271949cad139190d1c5239967e
Antonio Ruano <blinx2500@gmail.com>
2021-11-01 22:59:54 +0100

    Asignación valor de musica y sfx

--- Quiz/app/src/main/java/com/mrwojack/quiz/fragments/settings/MusicSettingsFragment.java
+++ Quiz/app/src/main/java/com/mrwojack/quiz/fragments/settings/MusicSettingsFragment.java
@@ -1,7 +1,11 @@
 package com.mrwojack.quiz.fragments.settings;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,6 +16,9 @@ import android.widget.SeekBar;
 import com.mrwojack.quiz.R;
 
 public class MusicSettingsFragment extends Fragment {
+
+    SeekBar musicBar;
+    SeekBar sfxBar;
 
     public MusicSettingsFragment() {
         // Required empty public constructor
@@ -27,5 +34,20 @@ public class MusicSettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_music_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        musicBar = view.findViewById(R.id.sb_Music);
+        sfxBar = view.findViewById(R.id.sb_Effects);
+
+        musicBar.setProgress(
+                getActivity().getSharedPreferences("preferenceias", Context.MODE_PRIVATE).getInt("musicVol", 0)
+        );
+        sfxBar.setProgress(
+                getActivity().getSharedPreferences("preferenceias", Context.MODE_PRIVATE).getInt("sfxVol", 0)
+        );
     }
 }
