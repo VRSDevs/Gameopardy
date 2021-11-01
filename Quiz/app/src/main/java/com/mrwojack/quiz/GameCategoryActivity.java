fd5ae704d8b93f0f793decbfe4a4551eebe32dbd
Antonio Ruano <blinx2500@gmail.com>
2021-11-02 00:49:47 +0100

    Creación actividad

---
+++ Quiz/app/src/main/java/com/mrwojack/quiz/GameCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.mrwojack.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_category);
+    }
+}
