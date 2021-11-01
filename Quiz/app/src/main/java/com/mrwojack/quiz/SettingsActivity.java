a1d80f5758961ae8c231d422986840dcada5d4da
Antonio Ruano <blinx2500@gmail.com>
2021-11-01 18:31:04 +0100

    Algoritmo para cambio de fragmento

--- Quiz/app/src/main/java/com/mrwojack/quiz/SettingsActivity.java
+++ Quiz/app/src/main/java/com/mrwojack/quiz/SettingsActivity.java
@@ -6,12 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+
+import com.mrwojack.quiz.fragments.settings.MusicSettingsFragment;
 
 public class SettingsActivity extends AppCompatActivity {
 
     /****************** VARIABLES *******************/
+
 
     /****************** CICLO DE VIDA *******************/
 
@@ -24,25 +29,6 @@ public class SettingsActivity extends AppCompatActivity {
     /****************** MÉTODOS DE NAVEGACIÓN *******************/
 
     public void showMusicSettings(View view) {
-        // VARIABLES //
-        // Variable de navegación entre fragmentos
-        final NavController NAV_CONTROLLER = Navigation.findNavController(view);
-        // Referencias a componentes de la vista
-        Button btn = view.findViewById(R.id.btt_Music);
-
-        // Asignación de evento de escucha al botón de comienzo
-        btn.setOnClickListener(new View.OnClickListener() {
-            /**
-             * Método ejecutado cuando se hace clic en el botón
-             * @param view -> Referencia a la vista
-             */
-            @Override
-            public void onClick(View view) {
-                // Navegación al fragmento
-                NAV_CONTROLLER.navigate(R.id.musicSettingsFragment);
-            }
-        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragCV_Settings, new MusicSettingsFragment()).commit();
     }
-
-
 }
